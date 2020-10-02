55f96daa1c6c12a2009a583aded123e0633ae025
Andrew A. Stenipanin <and5251@yandex.ru>
2020-10-02 14:34:50 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Jumbotron from './components/jumbotron/index'
+
+const App = () => {
+  return (
+    <Jumbotron />
+  );
+}
+
+export default App;
---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Inner } from './styles/jumbotron';
+
+const Jumbotron = ({}) => {
+  return ( 
+    <Inner>
+      <p>I am the jumbotron</p>
+    </Inner>
+   );
+}
+ 
+export default Jumbotron;
+
