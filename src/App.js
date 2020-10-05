8511b4c375ca51fc7b67539357da79ea83e37b21
Andrew A. Stenipanin <and5251@yandex.ru>
2020-10-05 22:48:10 +0300

    footer component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import React from "react";
-import jumboData from "./fixtures/jumbo";
-import Jumbotron from "./components/jumbotron/index";
+import JumbotronContainer from "../src/containers/jumbotron";
+import FooterContainer from "../src/containers/footer";
 
 const App = () => {
   return (
-    <Jumbotron.Container>
-      {jumboData.map((item) => (
-        <Jumbotron key={item.id} direction={item.direction}>
-          <Jumbotron.Pane>
-            <Jumbotron.Title>{item.title}</Jumbotron.Title>
-            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
-          </Jumbotron.Pane>
-          <Jumbotron.Image src={item.image} alt={item.alt} />
-        </Jumbotron>
-      ))}
-    </Jumbotron.Container>
+    <>
+      <JumbotronContainer />
+      <FooterContainer />
+    </>
   );
 };
 
