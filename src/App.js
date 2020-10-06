572fbe1e4dd381c23f9d8a992ba646ea6f1f741c
Andrew A. Stenipanin <and5251@yandex.ru>
2020-10-06 21:41:58 +0300

    accordion component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
-import JumbotronContainer from "../src/containers/jumbotron";
-import FooterContainer from "../src/containers/footer";
+import JumbotronContainer from "./containers/jumbotron";
+import FooterContainer from "./containers/footer";
+import FaqsContainer from "./containers/faqs";
 
 const App = () => {
   return (
     <>
       <JumbotronContainer />
+      <FaqsContainer />
       <FooterContainer />
     </>
   );
