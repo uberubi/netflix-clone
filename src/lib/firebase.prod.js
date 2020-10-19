3578e33a4f5445b866615ebe599ec663478566af
Andrew A. Stenipanin <and5251@yandex.ru>
2020-10-19 22:34:50 +0300

    setup firebase

---
+++ src/lib/firebase.prod.js
@@ -0,0 +1,22 @@
+import Firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+import { seedDatabase } from "../seed";
+// seed the database
+
+const config = {
+  apiKey: "AIzaSyAIyiun3Q8wXJrdI4uoPPvXRi6fKgE_qDY",
+  authDomain: "netflix-e00df.firebaseapp.com",
+  databaseURL: "https://netflix-e00df.firebaseio.com",
+  projectId: "netflix-e00df",
+  storageBucket: "netflix-e00df.appspot.com",
+  messagingSenderId: "764306484961",
+  appId: "1:764306484961:web:12979a2ce2e64203acd1a0",
+};
+
+const firebase = Firebase.initializeApp(config);
+
+// commented after succeed seeding
+// seedDatabase(firebase)
+
+export { firebase };
