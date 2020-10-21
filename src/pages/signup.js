cb16993e40ccb4b1942b07e31df31c4c5a301e87
Andrew A. Stenipanin <and5251@yandex.ru>
2020-10-22 01:40:14 +0300

    sign up page

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,7 +1,66 @@
-import React from 'react';
+import React, { useState, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import { FirebaseContext } from "../context/firebase";
+import HeaderContainer from "../containers/header";
+import FooterContainer from "../containers/footer";
+import { Form } from "../components";
+import * as ROUTES from "../constants/routes";
 
 const SignUp = () => {
-  return (<p>Hello from the sign up!</p>);
-}
- 
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+
+  const [firstName, setFirstName] = useState("");
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const isInvalid = firstName === "" || password === "" || emailAddress === "";
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign Up</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+          <Form.Base onSubmit={handleSignUp} method="POST">
+            <Form.Input
+              placeholder="First Name"
+              value={firstName}
+              onChange={({ target }) => setFirstName(target.value)}
+            />
+            <Form.Input
+              placeholder="Email address"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <Form.Input
+              type="password"
+              value={password}
+              autoComplete="off"
+              placeholder="Password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type="submit">
+              Sign Up
+            </Form.Submit>
+            <Form.Text>
+              Already a user? <Form.Link to="/signin">Sign In Now</Form.Link>
+            </Form.Text>
+            <Form.SmallText>
+              This page is protected by Google reCAPTCHA to ensure you're not a
+              bot. Learn more.
+            </Form.SmallText>
+          </Form.Base>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
+  );
+};
+
 export default SignUp;
